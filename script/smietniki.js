91908e0d5701c9f22da68872d2911dcb70615d5f
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2020-11-03 15:33:04 +0100

    Dodany drugi przykład tworzenia śmietnika

--- script/smietniki.js
+++ script/smietniki.js
@@ -1,6 +1,6 @@
 class Smietnik {
     constructor(type, pietro, place) {
-        // Typ śmietnika (papier, szkło, mieszany)
+        // Typ śmietnika (papier, plastik, bio)
         this.type = type;
         // Piętro na którym śmietnik stoi
         this.pietro = pietro;
@@ -12,3 +12,4 @@ class Smietnik {
 
 // Przykład jak stworzyć śmietnik
 const smietnikParter = new Smietnik('papier', 0, 1);
+const smietnikParter2 = new Smietnik('papier', 0, 2);
