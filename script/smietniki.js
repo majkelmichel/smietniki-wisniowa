d5cbdb61c2c3fa5b64b0141732481c5eab7da673
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2020-11-03 15:12:49 +0100

    Dodana klasa śmietników

---
+++ script/smietniki.js
@@ -0,0 +1,10 @@
+class Smietnik {
+    constructor(type, pietro, place) {
+        // Typ śmietnika (papier, szkło, mieszany)
+        this.type = type;
+        // Piętro na którym śmietnik stoi
+        this.pietro = pietro;
+        // Numer miejsca, w którym śmietnik stoi
+        this.place = place;
+    }
+}
